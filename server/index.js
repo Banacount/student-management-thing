2f6e61040af0f3e835a7cf2825ddc3b2f65406a7
Banacount <uniquejmal@gmail.com>
2026-08-10 19:05:45 +0800

    A3: server- mongoose testing, client- none

--- server/index.js
+++ server/index.js
@@ -1,17 +1,30 @@
 import express from 'express';
+import mongoose, { mongo } from 'mongoose';
 
 const server = {
 	app: express(),
-
-	config: {
-		port: 5959
-	}
 }
 const app = server.app;
 
-app.listen(server.config.port);
 
-app.get('/', (req, res) => {
-	res.send("Everybody starts somewhere, right?!");
-	console.log("Somebody visited, and we do start somewhere fasho!");
+mongoose.connect(process.env.MONGO_URI)
+	.then(() => {
+		console.log("Connected to the fukin database!");
+		app.listen(process.env.PORT || 5000);
+	})
+	.catch((error) => {
+		console.error("Error: ", error.message);
+		process.exit(1);
+	}
+)
+
+app.get('/', async (req, res) => { 
+	try {
+		const db = mongoose.connection.db;
+		const data = await db.collection('items').find({}).toArray();
+
+		res.json(data);
+	} catch (err) {
+		res.status(500).json({ fukin_error: err.message });
+	}
 });
